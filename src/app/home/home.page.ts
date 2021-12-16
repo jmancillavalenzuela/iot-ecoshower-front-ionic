313365b5a1b2dfeda22df225b8bc2c47cb31e593
JMancilla <juan.mancilla.94@gmail.com>
2021-12-16 19:57:25 -0300

    add: home Form Logic and route

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  leftImage: String;
-  ecoshowerLogo: String;
-  
-  constructor() {
-    this.leftImage = "./assets/images/left-image-login.png";
-    this.ecoshowerLogo = "./assets/logo/ecoshower.png";
+export class HomePage implements OnInit {
+  leftImage = "./assets/images/left-image-login.png";
+  ecoshowerLogo = "./assets/logo/ecoshower.png";
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  constructor(public router: Router) {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
   }
 
+  onRecovery() {
+    this.router.navigateByUrl('/recovery');
+  }
+
+  onRegister() {
+    this.router.navigateByUrl('/register');
+  }
 }
